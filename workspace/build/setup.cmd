18ef847c0424454fc41801085575199e4f89cff7
vngarla <vngarla@199a8c0d-f52f-db5a-f41a-c7518a5198ba>
2010-06-15 14:55:32 +0000

    updated

--- workspace/build/setup.cmd
+++ workspace/build/setup.cmd
@@ -2,6 +2,5 @@
 @rem wrapper to run build-setup.xml build script
 
 @call setenv.cmd
-@set CLASSPATH=%ANT_CONTRIB_HOME%\ant-contrib-1.0b3.jar
-%ANT_HOME%\bin\ant -Dmaveric.home=%MAVERIC_HOME% -Dytex.home=%YTEX_HOME% -buildfile build-setup.xml %*
+%ANT_HOME%\bin\ant -Dytex.home=%YTEX_HOME% -buildfile build-setup.xml %*
 @endlocal
