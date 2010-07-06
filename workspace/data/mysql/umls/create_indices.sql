13a27f64cedaf68fc3328fab94b0a295a7e80262
vngarla <vngarla@199a8c0d-f52f-db5a-f41a-c7518a5198ba>
2010-07-06 15:45:29 +0000

    Share project "data" into "https://ytex.googlecode.com/svn/trunk"

---
+++ workspace/data/mysql/umls/create_indices.sql
@@ -0,0 +1,11 @@
+create index sourcetypeidx on umls_ms_2009 (sourcetype);
+
+create index fwordidx on umls_ms_2009 (fword);
+
+create index cuiidx on umls_ms_2009 (cui);
+
+
+create index codemapidx on umls_snomed_map (code);
+
+create index cuimapidx on umls_snomed_map (cui);
+
