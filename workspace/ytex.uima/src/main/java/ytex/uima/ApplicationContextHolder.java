74857eabd8b4601d8cc70e0a91a836a31473b070
vngarla <vngarla@199a8c0d-f52f-db5a-f41a-c7518a5198ba>
2010-07-13 12:28:09 +0000

    updated

--- workspace/ytex.uima/src/main/java/ytex/uima/ApplicationContextHolder.java
+++ workspace/ytex.uima/src/main/java/ytex/uima/ApplicationContextHolder.java
@@ -1,5 +1,11 @@
 package ytex.uima;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.access.BeanFactoryLocator;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.access.ContextSingletonBeanFactoryLocator;
@@ -7,16 +13,48 @@ import org.springframework.context.access.ContextSingletonBeanFactoryLocator;
 /**
  * 
  * @author vijay
- * @TODO load beanRefContext.xml from ytex.properties.
  */
 public class ApplicationContextHolder {
-	private static final BeanFactoryLocator beanFactory = ContextSingletonBeanFactoryLocator
-			.getInstance();
-	private static final ApplicationContext ytexApplicationContext = (ApplicationContext) beanFactory
-			.useBeanFactory("ytexApplicationContext").getFactory();
+	private static final Log log = LogFactory
+			.getLog(ApplicationContextHolder.class);
+	private static Properties ytexProperties;
+	private static BeanFactoryLocator beanFactory;
+	private static ApplicationContext ytexApplicationContext;
+
+	static {
+		InputStream ytexPropsIn = null;
+		String beanRefContext = "classpath*:beanRefContext.xml";
+		try {
+			ytexPropsIn = ApplicationContextHolder.class
+					.getResourceAsStream("/ytex.properties");
+			ytexProperties = new Properties();
+			ytexProperties.load(ytexPropsIn);
+			beanRefContext = ytexProperties.getProperty("ytex.beanRefContext",
+					beanRefContext);
+		} catch (Exception e) {
+			log.error("initalizer", e);
+		} finally {
+			if (ytexPropsIn != null) {
+				try {
+					ytexPropsIn.close();
+				} catch (IOException e) {
+				}
+			}
+		}
+		if(log.isInfoEnabled())
+			log.info("beanRefContext="+beanRefContext);
+		beanFactory = ContextSingletonBeanFactoryLocator
+				.getInstance(beanRefContext);
+		ytexApplicationContext = (ApplicationContext) beanFactory
+				.useBeanFactory("ytexApplicationContext").getFactory();
+	}
 
 	public static ApplicationContext getApplicationContext() {
 		return ytexApplicationContext;
+	}
+
+	public static Properties getYtexProperties() {
+		return ytexProperties;
 	}
 
 }
