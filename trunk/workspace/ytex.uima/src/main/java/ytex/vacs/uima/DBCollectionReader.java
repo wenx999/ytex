13b8b8c36a3a94c38e6322f947ced1cd295ad8c5
vngarla <vngarla@199a8c0d-f52f-db5a-f41a-c7518a5198ba>
2010-07-26 17:10:10 +0000

    extensible dockey

--- trunk/workspace/ytex.uima/src/main/java/ytex/vacs/uima/DBCollectionReader.java
+++ trunk/workspace/ytex.uima/src/main/java/ytex/vacs/uima/DBCollectionReader.java
@@ -1,7 +1,7 @@
 package ytex.vacs.uima;
 
-
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
@@ -9,12 +9,15 @@ import java.util.Map;
 
 import javax.sql.DataSource;
 
+import org.apache.commons.beanutils.BeanUtils;
+import org.apache.commons.beanutils.ConstructorUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.uima.cas.CAS;
 import org.apache.uima.cas.CASException;
 import org.apache.uima.collection.CollectionException;
 import org.apache.uima.collection.CollectionReader_ImplBase;
+import org.apache.uima.jcas.tcas.Annotation;
 import org.apache.uima.resource.ResourceInitializationException;
 import org.apache.uima.resource.metadata.ConfigurationParameterSettings;
 import org.apache.uima.resource.metadata.ProcessingResourceMetaData;
@@ -31,16 +34,36 @@ import ytex.vacs.uima.types.DocumentKey;
 
 /**
  * 
- * Read documents from db.
+ * Read documents from db. Config parameters:
+ * <ul>
+ * <li>queryGetDocumentKeys the query to get the document keys</li>
+ * <li>queryGetDocument the query to get a document given a key. should have
+ * named parameters that match the columns of the result set returned by
+ * queryGetDocumentKeys</li>
+ * <li>keyTypeName the uima type of the document key to be added to the cas.
+ * defaults to ytex.vacs.uima.types.DocumentKey.
+ * </ul>
+ * 
  * @TODO more doc
  * @author vijay
- *
+ * 
  */
 public class DBCollectionReader extends CollectionReader_ImplBase {
 	private static final Log log = LogFactory.getLog(DBCollectionReader.class);
 
+	/**
+	 * the query to get the document keys set in config file
+	 */
 	String queryGetDocumentKeys;
+	/**
+	 * the queyr to get a document given a key. set in config file
+	 */
 	String queryGetDocument;
+	/**
+	 * the key type. if not set, will default to
+	 * ytex.vacs.uima.types.DocumentKey.
+	 */
+	String keyTypeName;
 
 	DataSource dataSource;
 	SimpleJdbcTemplate simpleJdbcTemplate;
@@ -58,6 +81,8 @@ public class DBCollectionReader extends CollectionReader_ImplBase {
 				.getParameterValue("queryGetDocumentKeys");
 		this.queryGetDocument = (String) paramSettings
 				.getParameterValue("queryGetDocument");
+		this.keyTypeName = (String) paramSettings
+				.getParameterValue("keyTypeName");
 		dataSource = (DataSource) ApplicationContextHolder
 				.getApplicationContext().getBean("dataSource");
 		simpleJdbcTemplate = new SimpleJdbcTemplate(dataSource);
@@ -89,22 +114,44 @@ public class DBCollectionReader extends CollectionReader_ImplBase {
 										rs, 1);
 								aCAS.setDocumentText(clobText);
 							} else {
-								log.error("Multiple documents for document key: " + id);
+								log
+										.error("Multiple documents for document key: "
+												+ id);
 							}
 						}
 					});
 			try {
-				DocumentKey docKey = new DocumentKey(aCAS.getJCas());
-				if(id.get("studyid") != null)
-					docKey.setStudyID((Integer) id.get("studyid"));
-				if(id.get("uid") != null)
-					docKey.setUid((Integer) id.get("uid"));
-				if(id.get("document_type_id") != null)
-					docKey.setDocumentType((Integer) id.get("document_type_id"));
-				if (id.get("site_id") != null)
-					docKey.setSiteID((String) id.get("site_id"));
-				docKey.addToIndexes();
+				if (keyTypeName != null) {
+					Annotation key = (Annotation)ConstructorUtils.invokeConstructor(Class
+							.forName(keyTypeName), aCAS.getJCas());
+					if (key instanceof DocumentKey) {
+						// backwards compatibility
+						DocumentKey docKey = (DocumentKey)key;
+						if (id.get("studyid") != null)
+							docKey.setStudyID((Integer) id.get("studyid"));
+						if (id.get("uid") != null)
+							docKey.setUid((Integer) id.get("uid"));
+						if (id.get("document_type_id") != null)
+							docKey.setDocumentType((Integer) id
+									.get("document_type_id"));
+						if (id.get("site_id") != null)
+							docKey.setSiteID((String) id.get("site_id"));
+					}
+					// just copy properties with the specified key names
+					BeanUtils.populate(key, id);
+					key.addToIndexes();
+				}
 			} catch (CASException ce) {
+				throw new CollectionException(ce);
+			} catch (InvocationTargetException ce) {
+				throw new CollectionException(ce);
+			} catch (IllegalAccessException ce) {
+				throw new CollectionException(ce);
+			} catch (NoSuchMethodException ce) {
+				throw new CollectionException(ce);
+			} catch (InstantiationException ce) {
+				throw new CollectionException(ce);
+			} catch (ClassNotFoundException ce) {
 				throw new CollectionException(ce);
 			}
 		} else {
