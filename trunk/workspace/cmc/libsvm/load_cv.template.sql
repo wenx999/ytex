e0a579a32f948b6d25ed5742246b46cbdb937ff1
vngarla@gmail.com <vngarla@gmail.com@199a8c0d-f52f-db5a-f41a-c7518a5198ba>
2011-02-03 03:22:48 +0000

    updated

---
+++ trunk/workspace/cmc/libsvm/load_cv.template.sql
@@ -0,0 +1,123 @@
+delete from weka_results 
+where experiment = '@EXPERIMENT@';
+
+load data local infile 'E:/projects/ytex/cmc/libsvm/@EXPERIMENT@/cv.txt'
+into table weka_results
+(label, cost, weight, measureNumSupportVectors, 
+num_true_positives, num_false_positives, num_true_negatives, num_false_negatives, IR_precision, IR_recall, F_measure,
+scutThreshold,
+scutTP, scutFP, scutTN, scutFN, scutPrecision, scutREcall, scutFMeasure
+)
+set experiment = '@EXPERIMENT@', key_scheme_options = 'libsvm'
+;
+
+drop table if exists best_f1;
+create temporary table best_f1 (
+    label varchar(10),
+    best_f1 double
+);
+
+
+insert into best_f1 (label, best_f1)
+select label, max(f1a)
+from
+(
+    select label, cost, weight, avg(F_measure) f1a
+    from weka_results
+    where experiment in ('@EXPERIMENT@')
+    group by label, cost, weight
+    order by label asc, avg(F_measure) desc, experiment asc, cost desc
+) s
+group by label
+;
+
+
+drop table if exists best_nsv;
+create temporary table best_nsv (
+    label varchar(10),
+    best_f1 double,
+    best_nsv int
+);
+
+insert into best_nsv
+select p.label, p.best_f1, round(min(nsva)) min_nsv
+from
+(
+    select label, cost, weight, avg(F_measure) f1a, avg(measureNumSupportVectors) nsva
+    from weka_results
+    where experiment in ('@EXPERIMENT@')
+    group by label, cost, weight
+) s inner join best_f1 p on s.label = p.label and s.f1a = p.best_f1
+group by p.label, p.best_f1
+;
+
+drop table if exists best_cost;
+create temporary table best_cost (
+    label varchar(10),
+    best_f1 double,
+    best_nsv int,
+    max_cost double,
+    min_cost double
+);
+
+insert into best_cost
+select p.label, p.best_f1, p.best_nsv, max(cost), min(cost)
+from
+(
+    select label, cost, weight, avg(F_measure) f1a, round(avg(measureNumSupportVectors)) nsva
+    from weka_results
+    where experiment in ('@EXPERIMENT@')
+    group by label, cost, weight
+) s inner join best_nsv p
+    on (s.label, s.f1a, s.nsva)  = (p.label, p.best_f1, p.best_nsv)
+group by p.label, p.best_f1
+;
+
+
+delete from libsvm_cv_best where experiment = '@EXPERIMENT@';
+
+insert into libsvm_cv_best
+select p.label, p.best_f1, p.best_nsv, p.max_cost, p.min_cost, min(weight), null, '@EXPERIMENT@' 
+from
+(
+    select label, cost, weight, avg(F_measure) f1a, round(avg(measureNumSupportVectors)) nsva
+    from weka_results
+    where experiment in ('@EXPERIMENT@')
+    group by label, cost, weight
+) s inner join best_cost p
+    on (s.label, s.f1a, s.nsva, s.cost)  = (p.label, p.best_f1, p.best_nsv, p.min_cost)
+group by p.label, p.best_f1
+;
+
+
+select lcb.* 
+from libsvm_cv_best lcb
+inner join
+(
+select label, cost, weight, experiment,
+	avg(scutFMeasure) sf1a, avg(scutFMeasure-F_Measure) sf1diff,  
+	avg(scutThreshold) scutThreshold, stddev_pop(scutThreshold) sdevScutThreshold
+from weka_results
+group by label, cost, weight, experiment
+) cv on (cv.label, cv.cost, cv.weight, cv.experiment) = (lcb.label, lcb.min_cost, lcb.weight, lcb.experiment)
+where cv.experiment = '@EXPERIMENT@'
+;
+
+/*
+ * use scut when the stdev of the scut threshold is < 0.10 (the threshold is stable)
+ * and if the average improvement in f-score is > 5%
+ */
+update libsvm_cv_best lcb
+inner join
+(
+select label, cost, weight, experiment,
+	avg(scutPrecision) sprec, avg(scutRecall) srec, avg(scutFMeasure) sf1a, 
+	avg(scutFMeasure-F_Measure) sf1diff, avg(scutThreshold) scutThreshold, stddev_pop(scutThreshold) sdevScutThreshold
+from weka_results
+group by label, cost, weight, experiment
+) cv on (cv.label, cv.cost, cv.weight, cv.experiment) = (lcb.label, lcb.min_cost, lcb.weight, lcb.experiment)
+set lcb.scutThreshold = cv.scutThreshold
+where sdevScutThreshold < 0.10
+and sf1diff > 0.05
+and lcb.experiment = '@EXPERIMENT@' 
+;
---
+++ trunk/workspace/cmc/libsvm/select_params.template.sql
@@ -0,0 +1,13 @@
+-- generate properties file which specifies optimal params per class for svm-train
+select concat(label, '.cost=', min_cost) prop
+from libsvm_cv_best
+union
+select concat(label, '.weight=', weight)
+from cmc_cv_best
+union
+select concat(label, '.scut=', scutThreshold)
+from cmc_cv_best
+where scutThreshold > 0
+and experiment = '@EXPERIMENT@'
+;
+
