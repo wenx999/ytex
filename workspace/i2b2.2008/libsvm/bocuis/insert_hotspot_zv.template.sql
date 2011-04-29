31990708b222b0b84fd5e14224b07b844e08e06c
vngarla@gmail.com <vngarla@gmail.com@199a8c0d-f52f-db5a-f41a-c7518a5198ba>
2011-04-29 01:34:39 +0000

    ok

---
+++ workspace/i2b2.2008/libsvm/bocuis/insert_hotspot_zv.template.sql
@@ -0,0 +1,21 @@
+/**
+ * insert zero vectors based on the specified cutoff
+ * export.cutoff will be replaced with a number
+ */
+insert into hotspot_zero_vector (name, label, instance_id, cutoff)
+select 'i2b2.2008-cui', disease, docId, @export.cutoff@
+from
+(
+	select a.disease, d.docId
+	from i2b2_2008_doc d
+	inner join i2b2_2008_anno a on d.docId = a.docId and a.source = 'intuitive'
+	left join
+	(
+		select label, instance_id, count(*) fc
+		from hotspot_feature_eval
+		where evaluation > @export.cutoff@
+		group by label, instance_id
+	) h on d.docId = h.instance_id and a.disease = h.label
+	where fc is null
+) s
+;
