e83a5d64381a6205b7e9ffdba2243d2fbb208529
vngarla@gmail.com <vngarla@gmail.com@199a8c0d-f52f-db5a-f41a-c7518a5198ba>
2011-09-18 03:23:01 +0000

    umls for orcl

--- workspace/data/orcl/umls/create_indices.sql
+++ workspace/data/orcl/umls/create_indices.sql
@@ -1,11 +1,1 @@
-create index sourcetypeidx on umls_ms_2009 (sourcetype);
-
-create index fwordidx on umls_ms_2009 (fword);
-
-create index fwordidxci on umls_ms_2009 (nlssort(fword,'NLS_SORT=BINARY_CI'));
-
-create index cuiidx on umls_ms_2009 (cui);
-
-create index codemapidx on umls_snomed_map (code);
-
-create index cuimapidx on umls_snomed_map (cui);
+create index fwordidx on umls_aui_fword (fword);
--- workspace/data/orcl/umls/umls_snomed_map.ctl
+++
@@ -1,8 +0,0 @@
-load data
-infile umls_snomed_map.txt
-into table umls_snomed_map
-fields terminated by '\t'
- (
-  cui CHAR TERMINATED BY WHITESPACE,
-  code CHAR TERMINATED BY WHITESPACE
-)
