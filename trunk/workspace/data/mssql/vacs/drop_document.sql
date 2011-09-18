d576af26606a80b6bc59dba5f496e601261c8abb
vngarla@gmail.com <vngarla@gmail.com@199a8c0d-f52f-db5a-f41a-c7518a5198ba>
2011-09-18 04:07:26 +0000

    remove vacs - obsolete update build for oracle

--- trunk/workspace/data/mssql/vacs/drop_document.sql
+++
@@ -1,6 +0,0 @@
-IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'$(db_schema).anno_docdate') AND type in (N'U'))
-	drop table $(db_schema).anno_docdate;
-go
-IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'$(db_schema).anno_dockey') AND type in (N'U'))
-	drop table $(db_schema).anno_dockey;
-go
--- trunk/workspace/data/mssql/vacs/drop_reference.sql
+++
@@ -1,5 +0,0 @@
--- drop 'reference' data
-IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'$(db_schema).ref_document_type') AND type in (N'U'))
-	drop table $(db_schema).ref_document_type
-go
-
