2486a64330cd4b4fa34c046eb597a03ef1cf97fb
vngarla@gmail.com <vngarla@gmail.com@199a8c0d-f52f-db5a-f41a-c7518a5198ba>
2011-11-05 22:40:35 +0000

    tokenize, stem lookup table save covered_text

--- trunk/workspace/ytex.model/src/main/java/ytex/model/DocumentAnnotation.java
+++ trunk/workspace/ytex.model/src/main/java/ytex/model/DocumentAnnotation.java
@@ -13,65 +13,67 @@ public class DocumentAnnotation implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	int documentAnnotationID;
-	Document document;
-	public UimaType getUimaType() {
-		return uimaType;
-	}
-
-	public void setUimaType(UimaType uimaType) {
-		this.uimaType = uimaType;
-	}
-
-
 	Integer begin;
+	String coveredText;
+	Document document;
+
+	int documentAnnotationID;
+
+
 	Integer end;
 	UimaType uimaType;
-
 	public DocumentAnnotation() {
 		super();
-	}
-
-	public int getDocumentAnnotationID() {
-		return documentAnnotationID;
-	}
-
-	public void setDocumentAnnotationID(int documentAnnotationID) {
-		this.documentAnnotationID = documentAnnotationID;
-	}
-
-	public Document getDocument() {
-		return document;
-	}
-
-	public void setDocument(Document document) {
-		this.document = document;
-	}
-
-	public Integer getBegin() {
-		return begin;
-	}
-
-	public void setBegin(Integer begin) {
-		this.begin = begin;
-	}
-
-	public Integer getEnd() {
-		return end;
-	}
-
-	public void setEnd(Integer end) {
-		this.end = end;
 	}
 	public DocumentAnnotation(UimaType uimaType, Document doc) {
 		this.uimaType = uimaType;
 		this.document = doc;
 	}
 
-//	public DocumentAnnotation(Annotation annotation, UimaType uimaType, Document doc) {
-//		this.uimaType = uimaType;
-//		this.document = doc;
-//		this.begin = annotation.getBegin();
-//		this.end = annotation.getEnd();
-//	}
+	public Integer getBegin() {
+		return begin;
+	}
+
+	public String getCoveredText() {
+		return coveredText;
+	}
+
+	public Document getDocument() {
+		return document;
+	}
+
+	public int getDocumentAnnotationID() {
+		return documentAnnotationID;
+	}
+
+	public Integer getEnd() {
+		return end;
+	}
+
+	public UimaType getUimaType() {
+		return uimaType;
+	}
+
+	public void setBegin(Integer begin) {
+		this.begin = begin;
+	}
+
+	public void setCoveredText(String coveredText) {
+		this.coveredText = coveredText;
+	}
+
+	public void setDocument(Document document) {
+		this.document = document;
+	}
+
+	public void setDocumentAnnotationID(int documentAnnotationID) {
+		this.documentAnnotationID = documentAnnotationID;
+	}
+
+	public void setEnd(Integer end) {
+		this.end = end;
+	}
+	public void setUimaType(UimaType uimaType) {
+		this.uimaType = uimaType;
+	}
 }
