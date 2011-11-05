756140c4fcc6642c5f89441399ea282a1da214b7
vngarla@gmail.com <vngarla@gmail.com@199a8c0d-f52f-db5a-f41a-c7518a5198ba>
2011-11-05 22:36:57 +0000

    load hyphen map

--- workspace/ytex.uima/src/main/test/ytex/tools/SetupAuiFirstWordTest.java
+++ workspace/ytex.uima/src/main/test/ytex/tools/SetupAuiFirstWordTest.java
@@ -1,5 +1,6 @@
 package ytex.tools;
 
+import ytex.umls.model.UmlsAuiFirstWord;
 import junit.framework.TestCase;
 
 public class SetupAuiFirstWordTest extends TestCase {
@@ -10,6 +11,13 @@ public class SetupAuiFirstWordTest extends TestCase {
 
 	public void testSetupAuiFirstWord() throws Exception {
 		new SetupAuiFirstWord();
+	}
+	
+	public void testStem() throws Exception {
+		SetupAuiFirstWord sa = new SetupAuiFirstWord();
+		UmlsAuiFirstWord fw = sa.tokenizeStr("A10773589", "Prostate-specific antigen");
+		System.out.println(fw.getTokenizedStr());
+		
 	}
 
 }
