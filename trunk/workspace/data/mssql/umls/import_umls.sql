340b0d50b69aaae0e4b54519d3800321a3025378
vngarla@gmail.com <vngarla@gmail.com@199a8c0d-f52f-db5a-f41a-c7518a5198ba>
2011-12-29 04:43:21 +0000

    umls setup for mssql

--- trunk/workspace/data/mssql/umls/import_umls.sql
+++ trunk/workspace/data/mssql/umls/import_umls.sql
@@ -4,35 +4,35 @@
 
 -- intentionally do not drop MRCONSO - if it exists then we should use it 
 -- instead of overwriting it
-CREATE TABLE $(db_schema).[MRCONSO](
-	[CUI] [nchar](8) NOT NULL,
-	[LAT] [nchar](3) NOT NULL,
-	[TS] [nchar](1) NOT NULL,
-	[LUI] [nvarchar](10) NOT NULL,
-	[STT] [nvarchar](3) NOT NULL,
-	[SUI] [nvarchar](10) NOT NULL,
-	[ISPREF] [nchar](1) NOT NULL,
-	[AUI] [nvarchar](9) NOT NULL,
-	[SAUI] [nvarchar](50) NULL,
-	[SCUI] [nvarchar](50) NULL,
-	[SDUI] [nvarchar](50) NULL,
-	[SAB] [nvarchar](20) NOT NULL,
-	[TTY] [nvarchar](20) NOT NULL,
-	[CODE] [nvarchar](50) NOT NULL,
-	[STR] [nvarchar](max) NOT NULL,
-	[SRL] [int] NOT NULL,
-	[SUPPRESS] [nchar](1) NOT NULL,
-	[CVF] [int] NULL
+CREATE TABLE  $(db_schema).MRCONSO (
+    CUI	char(8) NOT NULL,
+    LAT	char(3) NOT NULL,
+    TS	char(1) NOT NULL,
+    LUI	varchar(10) NOT NULL,
+    STT	varchar(3) NOT NULL,
+    SUI	varchar(10) NOT NULL,
+    ISPREF	char(1) NOT NULL,
+    AUI	varchar(9) NOT NULL,
+    SAUI	varchar(50),
+    SCUI	varchar(50),
+    SDUI	varchar(50),
+    SAB	varchar(20) NOT NULL,
+    TTY	varchar(20) NOT NULL,
+    CODE	varchar(50) NOT NULL,
+    STR	nvarchar(max) NOT NULL,
+    SRL	int  NOT NULL,
+    SUPPRESS	char(1) NOT NULL,
+    CVF	int 
 );
 
 ALTER TABLE $(db_schema).MRCONSO ADD CONSTRAINT X_MRCONSO_PK  PRIMARY KEY (AUI);
 
 CREATE TABLE  $(db_schema).MRSTY (
-    CUI	nchar(8) NOT NULL,
-    TUI	nchar(4) NOT NULL,
-    STN	nvarchar(100) NOT NULL,
-    STY	nvarchar(50) NOT NULL,
-    ATUI	nvarchar(11) NOT NULL,
+    CUI	char(8) NOT NULL,
+    TUI	char(4) NOT NULL,
+    STN	varchar(100) NOT NULL,
+    STY	varchar(50) NOT NULL,
+    ATUI	varchar(11) NOT NULL,
     CVF	int 
 );
 
