abce4b75192e6567feaaccbcdcb83192c0ccebff
vngarla@gmail.com <vngarla@gmail.com@199a8c0d-f52f-db5a-f41a-c7518a5198ba>
2012-06-11 02:24:39 +0000

    ok

--- trunk/projects/simbenchmark/data/ukb/setupcg.sh
+++ trunk/projects/simbenchmark/data/ukb/setupcg.sh
@@ -6,6 +6,6 @@
 # and ${1}.bin concept graph
 export UKB_HOME=${HOME}/bin/ukb-0.1.6
 
-mysql --user=ytex --password=ytex --database=umls2011ab --skip-column-names < ${1}.sql > ${1}.rel.txt
+mysql --user=ytex --password=ytex --database=${2} --skip-column-names < ${1}.sql > ${1}.rel.txt
 
 ${UKB_HOME}/bin/compile_kb -o ${1}.bin ${1}.rel.txt
--- trunk/projects/simbenchmark/data/ukb/sim.sh
+++ trunk/projects/simbenchmark/data/ukb/sim.sh
@@ -6,5 +6,6 @@ export UKB_HOME=${HOME}/bin/ukb-0.1.6
 cd ${UKB_HOME}/UKBsim
 PREFIX=$1
 KB=$2
+mkdir ${DATA_HOME}/ukb/${KB}
 
-cat ${DATA_HOME}/${PREFIX}_id.txt | ./similarity.pl --sim dot --ukbargs "--concept_graph --only_synsets --nopos --dict_file ${DATA_HOME}/ukb/${PREFIX}_dict.txt --kb_binfile ${DATA_HOME}/ukb/${KB}.bin" > ${DATA_HOME}/ukb/${PREFIX}_ppr.txt
+cat ${DATA_HOME}/${PREFIX}_id.txt | ./similarity.pl --sim dot --ukbargs "--concept_graph --only_synsets --nopos --dict_file ${DATA_HOME}/ukb/${PREFIX}_dict.txt --kb_binfile ${DATA_HOME}/ukb/${KB}.bin" > ${DATA_HOME}/ukb/${KB}/${PREFIX}_ppr.txt
